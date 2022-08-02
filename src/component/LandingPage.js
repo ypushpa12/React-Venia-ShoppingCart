6e10e1e8bda67ebe513d0ef15e055041da7a3e32
ypushpa12 <pushpalatha.yarra@gspann.com>
2022-08-02 14:30:59 +0530

    responsive

--- src/component/LandingPage.js
+++ src/component/LandingPage.js
@@ -16,16 +16,17 @@ export default function Landing() {
             <div class="aem-Grid aem-Grid--12">
               <div className="aem-GridColumn aem-GridColumn--default--6 aem-GridColumn--phone--12">
                 <div className='Landing_left'>
-                  <h2>Shop the new<br /> Women’s Collection</h2>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Lobortis mattis aliquam faucibus purus.</p>
-                  <button><p>SHOP NOW</p></button>
+                  <div className='left-content'>
+                    <h2>Shop the new<br /> Women’s Collection</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                      dolore magna aliqua. Lobortis mattis aliquam faucibus purus.</p>
+                    <button><p>SHOP NOW</p></button>
+                  </div>
                 </div>
-
               </div>
               <div class="aem-GridColumn aem-GridColumn--default--6 aem-GridColumn--phone--12">
                 <div className='Landing-right'>
-                  <img src="https://i.pinimg.com/originals/5f/d7/26/5fd726232a043b591df8272be0c4d0cd.jpg" />
+                  <img src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22980%22%20height%3D%22650%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20980%20650%22%20preserveAspectRatio%3D%22none%22%3E%0A%20%20%20%20%20%20%3Cdefs%3E%0A%20%20%20%20%20%20%20%20%3Cstyle%20type%3D%22text%2Fcss%22%3E%0A%20%20%20%20%20%20%20%20%20%20%23holder%20text%20%7B%0A%20%20%20%20%20%20%20%20%20%20%20%20fill%3A%20%23ffffff%3B%0A%20%20%20%20%20%20%20%20%20%20%20%20font-family%3A%20sans-serif%3B%0A%20%20%20%20%20%20%20%20%20%20%20%20font-size%3A%2040px%3B%0A%20%20%20%20%20%20%20%20%20%20%20%20font-weight%3A%20400%3B%0A%20%20%20%20%20%20%20%20%20%20%7D%0A%20%20%20%20%20%20%20%20%3C%2Fstyle%3E%0A%20%20%20%20%20%20%3C%2Fdefs%3E%0A%20%20%20%20%20%20%3Cg%20id%3D%22holder%22%3E%0A%20%20%20%20%20%20%20%20%3Crect%20width%3D%22100%25%22%20height%3D%22100%25%22%20fill%3D%22%23cccccc%22%3E%3C%2Frect%3E%0A%20%20%20%20%20%20%20%20%3Cg%3E%0A%20%20%20%20%20%20%20%20%20%20%3Ctext%20text-anchor%3D%22middle%22%20x%3D%2250%25%22%20y%3D%2250%25%22%20dy%3D%22.3em%22%3ECarousel%20Image%3C%2Ftext%3E%0A%20%20%20%20%20%20%20%20%3C%2Fg%3E%0A%20%20%20%20%20%20%3C%2Fg%3E%0A%20%20%20%20%3C%2Fsvg%3E" className='imgsize' />
 
                 </div>
               </div>
@@ -33,46 +34,70 @@ export default function Landing() {
           </Carousel.Item>
           <Carousel.Item>
             <div class="aem-Grid aem-Grid--12">
-              <div class="aem-GridColumn aem-GridColumn--default--6 aem-GridColumn--phone--12">
+              <div className="aem-GridColumn aem-GridColumn--default--6 aem-GridColumn--phone--12">
                 <div className='Landing_left'>
-                  <h2>Shop the new<br />Women’s Collection</h2>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Lobortis mattis aliquam faucibus purus.</p>
-                  <button><p>SHOP NOW</p></button>
+                  <div className='left-content'>
+                    <h2>Shop the new<br /> Women’s Collection</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                      dolore magna aliqua. Lobortis mattis aliquam faucibus purus.</p>
+                    <button><p>SHOP NOW</p></button>
+                  </div>
                 </div>
-
               </div>
               <div class="aem-GridColumn aem-GridColumn--default--6 aem-GridColumn--phone--12">
                 <div className='Landing-right'>
-                  <img src="https://thumbs.dreamstime.com/b/fashion-shopping-girl-portrait-yellow-background-shopping-concept-fashion-shopping-girl-portrait-yellow-background-186389790.jpg" />
+                  <img src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22980%22%20height%3D%22650%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20980%20650%22%20preserveAspectRatio%3D%22none%22%3E%0A%20%20%20%20%20%20%3Cdefs%3E%0A%20%20%20%20%20%20%20%20%3Cstyle%20type%3D%22text%2Fcss%22%3E%0A%20%20%20%20%20%20%20%20%20%20%23holder%20text%20%7B%0A%20%20%20%20%20%20%20%20%20%20%20%20fill%3A%20%23ffffff%3B%0A%20%20%20%20%20%20%20%20%20%20%20%20font-family%3A%20sans-serif%3B%0A%20%20%20%20%20%20%20%20%20%20%20%20font-size%3A%2040px%3B%0A%20%20%20%20%20%20%20%20%20%20%20%20font-weight%3A%20400%3B%0A%20%20%20%20%20%20%20%20%20%20%7D%0A%20%20%20%20%20%20%20%20%3C%2Fstyle%3E%0A%20%20%20%20%20%20%3C%2Fdefs%3E%0A%20%20%20%20%20%20%3Cg%20id%3D%22holder%22%3E%0A%20%20%20%20%20%20%20%20%3Crect%20width%3D%22100%25%22%20height%3D%22100%25%22%20fill%3D%22%23cccccc%22%3E%3C%2Frect%3E%0A%20%20%20%20%20%20%20%20%3Cg%3E%0A%20%20%20%20%20%20%20%20%20%20%3Ctext%20text-anchor%3D%22middle%22%20x%3D%2250%25%22%20y%3D%2250%25%22%20dy%3D%22.3em%22%3ECarousel%20Image%3C%2Ftext%3E%0A%20%20%20%20%20%20%20%20%3C%2Fg%3E%0A%20%20%20%20%20%20%3C%2Fg%3E%0A%20%20%20%20%3C%2Fsvg%3E" className='imgsize' />
 
                 </div>
               </div>
             </div>
           </Carousel.Item>
           <Carousel.Item>
-
             <div class="aem-Grid aem-Grid--12">
-              <div class="aem-GridColumn aem-GridColumn--default--6 aem-GridColumn--phone--12">
+              <div className="aem-GridColumn aem-GridColumn--default--6 aem-GridColumn--phone--12">
                 <div className='Landing_left'>
-                  <h2>Shop the new<br /> Women’s Collection</h2>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Lobortis mattis aliquam faucibus purus.</p>
-                  <button><p>SHOP NOW</p></button>
+                  <div className='left-content'>
+                    <h2>Shop the new<br /> Women’s Collection</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                      dolore magna aliqua. Lobortis mattis aliquam faucibus purus.</p>
+                    <button><p>SHOP NOW</p></button>
+                  </div>
                 </div>
-
               </div>
               <div class="aem-GridColumn aem-GridColumn--default--6 aem-GridColumn--phone--12">
                 <div className='Landing-right'>
-                  <img src="https://thumbs.dreamstime.com/b/full-size-photo-happy-excited-crazy-amazed-woman-shopaholic-hold-shopping-bags-isolated-yellow-color-background-233756884.jpg" />
+                  <img src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22980%22%20height%3D%22650%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20980%20650%22%20preserveAspectRatio%3D%22none%22%3E%0A%20%20%20%20%20%20%3Cdefs%3E%0A%20%20%20%20%20%20%20%20%3Cstyle%20type%3D%22text%2Fcss%22%3E%0A%20%20%20%20%20%20%20%20%20%20%23holder%20text%20%7B%0A%20%20%20%20%20%20%20%20%20%20%20%20fill%3A%20%23ffffff%3B%0A%20%20%20%20%20%20%20%20%20%20%20%20font-family%3A%20sans-serif%3B%0A%20%20%20%20%20%20%20%20%20%20%20%20font-size%3A%2040px%3B%0A%20%20%20%20%20%20%20%20%20%20%20%20font-weight%3A%20400%3B%0A%20%20%20%20%20%20%20%20%20%20%7D%0A%20%20%20%20%20%20%20%20%3C%2Fstyle%3E%0A%20%20%20%20%20%20%3C%2Fdefs%3E%0A%20%20%20%20%20%20%3Cg%20id%3D%22holder%22%3E%0A%20%20%20%20%20%20%20%20%3Crect%20width%3D%22100%25%22%20height%3D%22100%25%22%20fill%3D%22%23cccccc%22%3E%3C%2Frect%3E%0A%20%20%20%20%20%20%20%20%3Cg%3E%0A%20%20%20%20%20%20%20%20%20%20%3Ctext%20text-anchor%3D%22middle%22%20x%3D%2250%25%22%20y%3D%2250%25%22%20dy%3D%22.3em%22%3ECarousel%20Image%3C%2Ftext%3E%0A%20%20%20%20%20%20%20%20%3C%2Fg%3E%0A%20%20%20%20%20%20%3C%2Fg%3E%0A%20%20%20%20%3C%2Fsvg%3E" className='imgsize' />
 
                 </div>
               </div>
             </div>
-
           </Carousel.Item>
+
+
         </Carousel>
       </div>
+      {/* mobile */}
+      {/* <div class="container">
+
+        <div class="landing-mob">
+          <div class="aem-Grid aem-Grid--12">
+            <div class="aem-GridColumn aem-GridColumn--default--6 aem-GridColumn--phone--12">
+              <div className='Landingright'>
+                <img src="https://i.pinimg.com/originals/5f/d7/26/5fd726232a043b591df8272be0c4d0cd.jpg" />
+
+              </div>
+            </div>
+            <div class="aem-GridColumn aem-GridColumn--phone--12">
+              <div className='Landingleft'>
+                <h2>Shop the new<br /> Women’s Collection</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                  dolore magna aliqua. Lobortis mattis aliquam faucibus purus.</p>
+                <button><p>SHOP NOW</p></button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div> */}
       <div className='container'>
         <div className='landing_2'>
           <div class="aem-Grid aem-Grid--12">
@@ -83,8 +108,11 @@ export default function Landing() {
                 <div className='card-desc'>
                   <h3>Shop Women</h3>
                   <p>Lorem ipsum dolor sit amet</p>
-                </div>
 
+                </div>
+                <div className='lines'>
+                  <hr />
+                </div>
               </div>
             </div>
             <div class="aem-GridColumn aem-GridColumn--default--3 aem-GridColumn--phone--10">
@@ -97,7 +125,9 @@ export default function Landing() {
                   <p>Lorem ipsum dolor sit amet</p>
                 </div>
               </div>
-
+              <div className='lines'>
+                <hr />
+              </div>
 
               {/* </div> */}
             </div>
@@ -112,6 +142,9 @@ export default function Landing() {
 
 
               </div>
+              <div className='lines'>
+                <hr />
+              </div>
             </div>
             <div class="aem-GridColumn aem-GridColumn--default--3 aem-GridColumn--phone--10">
               <div className='landimg3'>
@@ -123,6 +156,9 @@ export default function Landing() {
 
                 </div>
 
+              </div>
+              <div className='lines'>
+                <hr />
               </div>
             </div>
           </div>
@@ -136,8 +172,8 @@ export default function Landing() {
               <div className='Landing_left'>
                 <h2>Take off in the new <br />Signature Legging</h2>
                 <h5>Lorem Ipsum Dolor Tempor</h5>
-                <p>Lorem ipsum dolor sit amet, consectetur adi<br/>piscing elit,sed do eiusmod tempor incididunt<br/>ut labore et
-                  dolore magna aliqua.Lobortis <br/>mattis aliquam faucibus purus.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adi<br />piscing elit,sed do eiusmod tempor incididunt<br />ut labore et
+                  dolore magna aliqua.Lobortis <br />mattis aliquam faucibus purus.</p>
                 <div className='btns'>
                   <button className='btn_1'><p>SHOP COLLECTION</p></button>
                   <button className='btn_2'><p>SHOP NOW</p></button>
@@ -156,7 +192,7 @@ export default function Landing() {
         </div>
       </div>
       {/* mobile */}
-      {/* <div className='container'>
+      <div className='container'>
         <div className='mob-landing'>
           <div class="aem-Grid aem-Grid--12">
             <div class="aem-GridColumn aem-GridColumn--default--6 aem-GridColumn--phone--12">
@@ -171,10 +207,19 @@ export default function Landing() {
                 <h5>Lorem Ipsum Dolor Tempor</h5>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
                   dolore magna aliqua. Lobortis mattis aliquam faucibus purus.</p>
-                <div className='btns'>
-                  <button className='btn_1'><p>SHOP COLLECTION</p></button>
-                  <button className='btn_2'><p>SHOP NOW</p></button>
-                  <hr />
+                <div class="aem-Grid aem-Grid--12">
+                  <div class="aem-GridColumn aem-GridColumn--default--6 aem-GridColumn--phone--12">
+                    <button className='btn_4'><p>SHOP NOW</p></button>
+                  </div>
+                  <div class="aem-GridColumn aem-GridColumn--default--6 aem-GridColumn--phone--12">
+                    <div className='btns'>
+                      <button className='btn_3'><p>SHOP COLLECTION</p></button>
+                    </div>
+
+                    <div className='line'>
+                      <hr />
+                    </div>
+                  </div>
                 </div>
               </div>
 
@@ -182,7 +227,7 @@ export default function Landing() {
 
           </div>
         </div>
-      </div> */}
+      </div>
 
 
       <div className='container'>
