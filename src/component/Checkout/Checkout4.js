59f01c0455febe4a60e679ae22193d49131fb2f7
ypushpa12 <pushpalatha.yarra@gspann.com>
2022-08-10 13:00:21 +0530

    changes to checkout

--- src/component/Checkout/Checkout4.js
+++ src/component/Checkout/Checkout4.js
@@ -103,28 +103,21 @@ function Review(props) {
                                 <div className='shipmethods__info-details totalitems'>
                                     <h6>3 items in your order</h6>
                                 </div>
-                                <div class="aem-GridColumn aem-GridColumn--default--6 aem-GridColumn--phone--12">
+                                {cartItems?.map((item) => {
+                                    return(
+                                    <div class="aem-GridColumn aem-GridColumn--default--6 aem-GridColumn--phone--12">
                                     <div className='items-inorder'>
-                                        <img src="https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg" />
+                                        <img src={item.image} />
                                         <div>
                                             <h6>Electric Leggings</h6>
                                             <p>Size: Medium</p>
                                             <p>Color: Storm</p>
-                                            <p>Quantity: 2</p>
+                                            <p>Quantity: {item.qty}</p>
                                         </div>
                                     </div>
-                                </div>
-                                <div class="aem-GridColumn aem-GridColumn--default--6 aem-GridColumn--phone--12">
-                                    <div className='items-inorder'>
-                                        <img src="https://fakestoreapi.com/img/51eg55uWmdL._AC_UX679_.jpg" />
-                                        <div>
-                                            <h6>Signature Sports Bra</h6>
-                                            <p>Size: Medium</p>
-                                            <p>Color: Red</p>
-                                            <p>Quantity: 1</p>
-                                        </div>
-                                    </div>
-                                </div>
+                                    </div>)
+                                })}
+                                
                             </div>
                         </div>
                         <div className='checkout-buttons'>
